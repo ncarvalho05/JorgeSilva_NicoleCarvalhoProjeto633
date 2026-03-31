2f2da867690b58632c930c31b42d661384c802d2
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-03-31 07:57:54 +0100

    Create GestaoProcessos.ps1 for process monitoring
    Added a script to monitor and manage top RAM-consuming processes.

---
+++ Scripts Projeto/GestaoProcessos.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+Write-Host "--- MONITORIZAÇÃO DE PROCESSOS (TOP 10 RAM) ---" -ForegroundColor Yellow
+ 
+Get-Process | Sort-Object WS -Descending | Select-Object -First 10 Name, ID, 
+    @{Name="Memoria(MB)"; Expression={ "{0:N2}" -f ($_.WS / 1MB) }} | Format-Table
+ 
+$idProc = Read-Host "Introduza o ID do processo para encerrar (ou Enter para sair)"
+ 
+if ($idProc -ne "") {
+    Stop-Process -Id $idProc -Force
+    Write-Host "Processo $idProc encerrado com sucesso!" -ForegroundColor Green
+}
